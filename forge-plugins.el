1bc7f5088be9e2a0c9a6ee84e4a89cc0b707760c
drlkf <drlkf@drlkf.net>
2026-06-13 23:13:50 +0200

    feat: add main `forge-plugin` module

---
+++ forge-plugins.el
@@ -0,0 +1,33 @@
+;;; forge-plugins.el --- Collection of plugins for forge  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  drlkf
+
+;; Author: drlkf
+;; Package-Requires: ((forge "0.5.0") (emacs "29.1"))
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package provides a collection of plugins that extend
+;; `forge'.  Loading this file enables all available plugins;
+;; individual plugins can also be loaded separately.
+
+;;; Code:
+
+(require 'forge-plugin-topic-format)
+
+(provide 'forge-plugins)
+;;; forge-plugins.el ends here
